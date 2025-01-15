312cf5136c9f4279daa7e2a00e990c48ae894f51
CorruptedGlitch1309 <113553528+CorruptedGlitch1309@users.noreply.github.com>
2025-01-14 18:58:50 -0600

    Import error

--- app/lib/client_actions.ts
+++ app/lib/client_actions.ts
@@ -1,5 +1,3 @@
-import { fetchMessages } from "./data";
-
 
 export function getServerInfo(servers: Array<any>, server_id: string) {
     console.log(servers.filter((server) => server_id == server.server_id))
