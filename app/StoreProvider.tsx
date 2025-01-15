b98ab25fed4347a1d62223ad16c01614ccab771e
CorruptedGlitch1309 <113553528+CorruptedGlitch1309@users.noreply.github.com>
2025-01-14 18:57:19 -0600

    Fixed use Ref

--- app/StoreProvider.tsx
+++ app/StoreProvider.tsx
@@ -4,12 +4,14 @@ import { useRef } from "react";
 import { makeStore } from "./lib/store";
 import { Provider } from "react-redux";
 
+type AppStore = ReturnType<typeof makeStore>;
+
 export default function StoreProvider({
   children,
 }: {
   children: Readonly<React.ReactNode>;
 }) {
-  const storeRef = useRef();
+  const storeRef = useRef<AppStore | null>(null);
   if (!storeRef.current) {
     storeRef.current = makeStore();
   }
